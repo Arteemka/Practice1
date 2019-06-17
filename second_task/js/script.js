be5fcb808ca2affeac0c371395899b1cfdd6d921
Artemka <artemiy@example.com>
2019-06-17 17:12:34 +0300

    second

--- second_task/js/script.js
+++ second_task/js/script.js
@@ -1,23 +1,39 @@
+let first = document.getElementById('first'),
+    second = document.getElementById('second');
+
 //first task
-const concatArguments = (...args) => { console.log(args.join('')); };
-concatArguments('Рак', 'Артемий', 'Витальевич');
-
-//second task
-
-
-const getNumberProperties = (obj) => {
-    for (var value in obj) {
-        if (typeof obj[value] === 'number') {
-            console.log(value + ':' + obj[value]);
-
-        }
+const concatArguments = (...args) => {
+    if (args.length == 0) {
+        console.log(args);
+    } else {
+        console.log(args.join(''));
     }
 };
-const numberProperties = {
+
+first.addEventListener('click', () => {
+    concatArguments('Рак', 'Артемий', 'Витальевич');
+});
+
+
+
+//second task
+const getNumberProperties = (obj) => {
+    let numberObject = {};
+
+    for (var value in obj) {
+        if (typeof obj[value] === 'number') {
+            numberObject[value] = obj[value];
+        }
+    }
+    console.log(numberObject);
+};
+const typicalObject = {
     a: 100,
     b: '100',
     c: true,
     d: 12.3
 };
 
-getNumberProperties(numberProperties);
+second.addEventListener('click', () => {
+    getNumberProperties(typicalObject);
+});
